84b4e6a3b9e758b7a7b44fe40245e0160ab0cca9
Mahamadou Kabba <mkabba34@gmail.com>
2023-04-26 10:53:50 -0400

    added nuget ps to csproj

--- API/Entities/lambda_handler.py
+++ API/Entities/lambda_handler.py
@@ -1,23 +1,23 @@
-from __future__ import print_function
-import json
-import urllib
-import boto
-import datetime
+# from __future__ import print_function
+# import json
+# import urllib
+# import boto
+# import datetime
 
-print('Loading function...')
+# print('Loading function...')
 
-def process_refund(message, context):
-    # Input Example 'key':'value'
-    # {'Transaction': 'PURCHASE'}
+# def process_refund(message, context):
+#     # Input Example 'key':'value'
+#     # {'Transaction': 'PURCHASE'}
 
-    # 1. Log input message
-    print("Received message from step Function:")
-    print(message)
+#     # 1. Log input message
+#     print("Received message from step Function:")
+#     print(message)
 
-    # 2. Construct response object
-    response = {}
-    response['TransectionType'] = message['Transection']
-    response['Timestamp'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    response['Message'] = 'Hello from lambda land inside the ProcessReund function'
+#     # 2. Construct response object
+#     response = {}
+#     response['TransectionType'] = message['Transection']
+#     response['Timestamp'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+#     response['Message'] = 'Hello from lambda land inside the ProcessReund function'
 
-    return response
+#     return response
